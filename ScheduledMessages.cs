f7c43dd5a2f9d10b984e6da7ebf92b3d685519e8
Peter <petergorman11@gmail.com>
2021-01-22 21:41:41 -0500

    Fix error in sequential scheduled messages being broadcasted

--- ScheduledMessages.cs
+++ ScheduledMessages.cs
@@ -10,7 +10,7 @@ using Oxide.Game.Rust.Libraries;
 
 namespace Oxide.Plugins
 {
-    [Info("Scheduled Messages", "gunman435", "1.0.0")]
+    [Info("Scheduled Messages", "gunman435", "1.0.1")]
     [Description("Allows the creation of custom messages to broadcast to players.")]
     class ScheduledMessages : CovalencePlugin
     {
@@ -240,7 +240,7 @@ namespace Oxide.Plugins
             {
                 // Get next message index.
                 nextMessage++;
-                if (nextMessage > config.scheduledMessages.Count)
+                if (nextMessage >= config.scheduledMessages.Count)
                     nextMessage = 0;
 
                 // Get next message.
